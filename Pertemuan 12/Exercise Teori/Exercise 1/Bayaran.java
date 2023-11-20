a436f62b1e6c779ec695a87883e76470a994af60
NajwaaAzzahra <najwaazzahra0507@gmail.com>
2023-11-20 21:48:23 +0700

    Pertemuan 12 Polimorfisme

--- Pertemuan 12/Exercise Teori/Exercise 1/Bayaran.java
+++ Pertemuan 12/Exercise Teori/Exercise 1/Bayaran.java
@@ -2,11 +2,11 @@ public class Bayaran {
     public int hitungBayaran (Pegawai pg) {
         int uang=pg.getGaji();
 
-        if(pg instanceof Manajer) {
-            uang += ((Manajer)pg).getTunjangan();
-        } else if (pg instanceof Manajer) {
-            uang += ((Programmer)pg).getBonus();
-        }
+        if (pg instanceof Manajer) {
+            uang += ((Manajer) pg).getTunjangan();
+        } else if (pg instanceof Programmer) {
+            uang += ((Programmer) pg).getBonus();
+        }       
         return uang;
     }
 }
